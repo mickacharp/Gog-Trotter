3581b95a80fd6b9ad6d9ce6295e0a8a507cf6d93
mickacharp <mickaelcharp@gmail.com>
2022-06-29 18:35:20 +0200

    Maps now generated with @angular/google-maps

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { GoogleMap } from '@capacitor/google-maps';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-explore-container',
@@ -7,83 +7,130 @@ import { GoogleMap } from '@capacitor/google-maps';
   styleUrls: ['./explore-container.component.scss'],
 })
 export class ExploreContainerComponent implements OnInit {
-  @Input() name: string;
-
-  apiKey = '';
-
   constructor() {}
 
+  title = 'angular-google-maps-app';
+
+  @ViewChild('myGoogleMap')
+  map!: GoogleMap;
+  @ViewChild(MapInfoWindow)
+  info!: MapInfoWindow;
+
+  zoom: number = 12;
+  maxZoom: number = 15;
+  minZoom: number = 8;
+  center!: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    zoomControl: true,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    mapTypeId: 'hybrid',
+    maxZoom: this.maxZoom,
+    minZoom: this.minZoom,
+  };
+  markers: any[] = [];
+  infoContent: string = '';
+
   ngOnInit() {
-    this.initMap();
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
   }
 
-  // mapRef = document.getElementById('map');
-  // newMap = GoogleMap.create({
-  //   id: 'my-map', // Unique identifier for this map instance
-  //   element: this.mapRef, // reference to the capacitor-google-map element
-  //   apiKey: this.apiKey, // Your Google Maps API Key
-  //   config: {
-  //     center: {
-  //       // The initial position to be rendered by the map
-  //       lat: 43.48,
-  //       lng: -1.56,
-  //     },
-  //     zoom: 12, // The initial zoom level to be rendered by the map
-  //   },
-  // });
+  eventHandler(event: any, name: string) {
+    console.log(name);
 
-  map: google.maps.Map;
-  service: google.maps.places.PlacesService;
-  infowindow: google.maps.InfoWindow;
-
-  initMap(): void {
-    const sydney = new google.maps.LatLng(-33.867, 151.195);
-
-    this.infowindow = new google.maps.InfoWindow();
-
-    this.map = new google.maps.Map(
-      document.getElementById('map') as HTMLElement,
-      {
-        center: sydney,
-        zoom: 15,
-      }
-    );
-
-    const request = {
-      query: 'Etxeko Pizza',
-      fields: ['name', 'geometry'],
-    };
-
-    this.service = new google.maps.places.PlacesService(this.map);
-
-    this.service.findPlaceFromQuery(
-      request,
-      (
-        results: google.maps.places.PlaceResult[] | null,
-        status: google.maps.places.PlacesServiceStatus
-      ) => {
-        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-          for (let i = 0; i < results.length; i++) {
-            this.createMarker(results[i]);
-          }
-
-          this.map.setCenter(results[0].geometry!.location!);
-        }
-      }
-    );
+    // Add marker on double click event
+    if (name === 'mapDblclick') {
+      this.dropMarker(event);
+    }
   }
 
-  createMarker(place: google.maps.places.PlaceResult) {
-    if (!place.geometry || !place.geometry.location) return;
+  // Markers
+  logCenter() {
+    console.log(JSON.stringify(this.map.getCenter()));
+  }
 
-    const marker = new google.maps.Marker({
-      map: this.map,
-      position: place.geometry.location,
+  dropMarker(event: any) {
+    this.markers.push({
+      position: {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      },
+      label: {
+        color: 'blue',
+        text: 'Marker label ' + (this.markers.length + 1),
+      },
+      title: 'Marker title ' + (this.markers.length + 1),
+      info: 'Marker info ' + (this.markers.length + 1),
+      options: {
+        animation: google.maps.Animation.DROP,
+      },
     });
+  }
 
-    google.maps.event.addListener(marker, 'click', () => {
-      this.infowindow.setContent(place.name || '');
-      this.infowindow.open(this.map);
-    });
+  openInfo(marker: MapMarker, content: string) {
+    this.infoContent = content;
+    this.info.open(marker);
   }
 }
+
+// GOOGLE TYPESCRIPT DOCS MAPS
+
+// map: google.maps.Map;
+// service: google.maps.places.PlacesService;
+// infowindow: google.maps.InfoWindow;
+
+// initMap(): void {
+//   const sydney = new google.maps.LatLng(-33.867, 151.195);
+
+//   this.infowindow = new google.maps.InfoWindow();
+
+//   this.map = new google.maps.Map(
+//     document.getElementById('map') as HTMLElement,
+//     {
+//       center: sydney,
+//       zoom: 15,
+//     }
+//   );
+
+//   const request = {
+//     query: 'Etxeko Pizza',
+//     fields: ['name', 'geometry'],
+//   };
+
+//   this.service = new google.maps.places.PlacesService(this.map);
+
+//   this.service.findPlaceFromQuery(
+//     request,
+//     (
+//       results: google.maps.places.PlaceResult[] | null,
+//       status: google.maps.places.PlacesServiceStatus
+//     ) => {
+//       if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+//         for (let i = 0; i < results.length; i++) {
+//           this.createMarker(results[i]);
+//         }
+
+//         this.map.setCenter(results[0].geometry!.location!);
+//       }
+//     }
+//   );
+// }
+
+// createMarker(place: google.maps.places.PlaceResult) {
+//   if (!place.geometry || !place.geometry.location) return;
+
+//   const marker = new google.maps.Marker({
+//     map: this.map,
+//     position: place.geometry.location,
+//   });
+
+//   google.maps.event.addListener(marker, 'click', () => {
+//     this.infowindow.setContent(place.name || '');
+//     this.infowindow.open(this.map);
+//   });
+// }
--- src/app/explore-container/explore-container.module.ts
+++ src/app/explore-container/explore-container.module.ts
@@ -1,13 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { GoogleMapsModule } from '@angular/google-maps';
 import { IonicModule } from '@ionic/angular';
 
 import { ExploreContainerComponent } from './explore-container.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule],
+  imports: [CommonModule, FormsModule, IonicModule, GoogleMapsModule],
   declarations: [ExploreContainerComponent],
   exports: [ExploreContainerComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
